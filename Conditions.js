d20995b8dc07a64c2ca733d9622864f1fcf7dfb4
Malik Waseem <malikwaseem123456564@gmail.com>
2026-01-30 15:04:30 +0500

    add

--- Conditions.js
+++ Conditions.js
@@ -1,5 +1,5 @@
 // Q1.check valid voter use ternary operator.
 let age=Number(prompt("Enter your age"));
 let result=age>=18?"valid voter":"invalid voter";
-console.log(result);dw
-j
+console.log(result);
+
