13369f11f3689b112cc46ddb67c5693f8397c985
Malik Waseem <malikwaseem123456564@gmail.com>
2026-01-31 11:34:14 +0500

    add pracrice questions

--- Conditions.js
+++ Conditions.js
@@ -1,5 +1,29 @@
-// Q1.check valid voter use ternary operator.
+// // Q1.check valid voter.
+
 let age=Number(prompt("Enter your age"));
-let result=age>=18?"valid voter":"invalid voter";
-console.log(result);
+
+if(isNaN(age))   {
+    console.log("wrong input");   
+}else if(age>=18){
+console.log("valid voter");
+
+}else{
+ console.log("invalid voter")
+}
+
+// Q2.Shope discount.
+let aitemprice=Number(prompt("enter price of item"));
+let discount=0;
+if(aitemprice>=0 && aitemprice<=5000){
+    discount=0;
+}  else if(aitemprice>5000 && aitemprice<=6000){
+    discount=10;
+}else if(aitemprice>6000 && aitemprice<=7000){
+    discount=20;
+}else if(aitemprice>7000){
+    discount=30;
+}else{
+    console.log("wrong value");
+}
+console.log(Math.floor(aitemprice-( aitemprice/100*discount)));
 
