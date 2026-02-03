d83a3eed2705346b06160433e5fb1e27ce72d4f9
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-03 12:20:35 +0500

    add practice question

--- Conditions.js
+++ Conditions.js
@@ -27,3 +27,58 @@ if(aitemprice>=0 && aitemprice<=5000){
 }
 console.log(Math.floor(aitemprice-( aitemprice/100*discount)));
 
+// Q3.calculate electricity bill.
+
+let units=Number(prompt("enter price of item"));
+let amount=0;
+if(units>400){
+    amount=(units-400)*16;
+    units=400;
+}
+if(units>200 &&units<=400){
+    amount+=(units-200)*12;//2400+800+400
+    units=200;
+}
+if(units>100 &&units<=200){
+    amount+=(units-100)*8;
+    units=100;
+}
+amount+=units*4;
+console.log(amount);
+
+// Q.4 PKR Dinomination.
+let amount=Number(prompt("Enter Amount "));
+if(amount>=1000){
+    console.log(Math.floor(amount/500));
+    amount=amount%500;
+}if(amount>=500){
+    console.log(amount/100);
+    amount=amount%100;
+}
+if(amount>=100){
+    console.log(amount/50);
+    amount=amount%50;
+}
+if(amount>=50){
+    console.log(amount/5);
+    amount=amount%5;
+}
+if(amount>=5){
+    console.log(amount/2);
+    amount=amount%2;
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
