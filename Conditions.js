8cc9733b4585c3588488509acf33eaa595e63454
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-04 11:18:53 +0500

    add questions

--- Conditions.js
+++ Conditions.js
@@ -1,72 +1,72 @@
-// // Q1.check valid voter.
+// // // Q1.check valid voter.
 
-let age=Number(prompt("Enter your age"));
+// let age=Number(prompt("Enter your age"));
 
-if(isNaN(age))   {
-    console.log("wrong input");   
-}else if(age>=18){
-console.log("valid voter");
+// if(isNaN(age))   {
+//     console.log("wrong input");   
+// }else if(age>=18){
+// console.log("valid voter");
 
-}else{
- console.log("invalid voter")
-}
+// }else{
+//  console.log("invalid voter")
+// }
 
-// Q2.Shope discount.
-let aitemprice=Number(prompt("enter price of item"));
-let discount=0;
-if(aitemprice>=0 && aitemprice<=5000){
-    discount=0;
-}  else if(aitemprice>5000 && aitemprice<=6000){
-    discount=10;
-}else if(aitemprice>6000 && aitemprice<=7000){
-    discount=20;
-}else if(aitemprice>7000){
-    discount=30;
-}else{
-    console.log("wrong value");
-}
-console.log(Math.floor(aitemprice-( aitemprice/100*discount)));
+// // Q2.Shope discount.
+// let aitemprice=Number(prompt("enter price of item"));
+// let discount=0;
+// if(aitemprice>=0 && aitemprice<=5000){
+//     discount=0;
+// }  else if(aitemprice>5000 && aitemprice<=6000){
+//     discount=10;
+// }else if(aitemprice>6000 && aitemprice<=7000){
+//     discount=20;
+// }else if(aitemprice>7000){
+//     discount=30;
+// }else{
+//     console.log("wrong value");
+// }
+// console.log(Math.floor(aitemprice-( aitemprice/100*discount)));
 
-// Q3.calculate electricity bill.
+// // Q3.calculate electricity bill.
 
-let units=Number(prompt("enter price of item"));
-let amount=0;
-if(units>400){
-    amount=(units-400)*16;
-    units=400;
-}
-if(units>200 &&units<=400){
-    amount+=(units-200)*12;//2400+800+400
-    units=200;
-}
-if(units>100 &&units<=200){
-    amount+=(units-100)*8;
-    units=100;
-}
-amount+=units*4;
-console.log(amount);
+// let units=Number(prompt("enter price of item"));
+// let amount=0;
+// if(units>400){
+//     amount=(units-400)*16;
+//     units=400;
+// }
+// if(units>200 &&units<=400){
+//     amount+=(units-200)*12;//2400+800+400
+//     units=200;
+// }
+// if(units>100 &&units<=200){
+//     amount+=(units-100)*8;
+//     units=100;
+// }
+// amount+=units*4;
+// console.log(amount);
 
-// Q.4 PKR Dinomination.
-let amount=Number(prompt("Enter Amount "));
-if(amount>=1000){
-    console.log(Math.floor(amount/500));
-    amount=amount%500;
-}if(amount>=500){
-    console.log(amount/100);
-    amount=amount%100;
-}
-if(amount>=100){
-    console.log(amount/50);
-    amount=amount%50;
-}
-if(amount>=50){
-    console.log(amount/5);
-    amount=amount%5;
-}
-if(amount>=5){
-    console.log(amount/2);
-    amount=amount%2;
-}
+// // Q.4 PKR Dinomination.
+// let amount=Number(prompt("Enter Amount "));
+// if(amount>=1000){
+//     console.log(Math.floor(amount/500));
+//     amount=amount%500;
+// }if(amount>=500){
+//     console.log(amount/100);
+//     amount=amount%100;
+// }
+// if(amount>=100){
+//     console.log(amount/50);
+//     amount=amount%50;
+// }
+// if(amount>=50){
+//     console.log(amount/5);
+//     amount=amount%5;
+// }
+// if(amount>=5){
+//     console.log(amount/2);
+//     amount=amount%2;
+// }
 
 
  
---
+++ Loops.js
@@ -0,0 +1,21 @@
+// Q1.sum of n natural numbers.
+let prime=prompt("enter a natural number");
+if(prime===null){
+    console.log("canceled");
+}
+else{ 
+    let n=Number(prime);
+    if(isNaN(prime)){
+    console.log("invalid number")
+}else if(prime<0){
+    console.log("it is negitive number not natural number")
+}
+else{
+    sum=0;
+    for(let i=0;i<=prime;i++){
+        sum+=i;
+    }
+    console.log(sum);
+}
+}
+
