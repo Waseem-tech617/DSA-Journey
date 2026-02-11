191d6331c08c5aa51a9a552d716452586f76a0a9
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-11 12:24:28 +0500

    add patterns

--- pattern.js
+++ pattern.js
@@ -1,7 +1,8 @@
 let prompt=require('prompt-sync')();
-let n=prompt("enter a number");
+let n=Number(prompt("enter a number"));
 
-// Q1.Right Pattern;
+// Q1.Right triangle;
+
 for(let i=1;i<=n;i++){
  for(let j=1;j<=i;j++){
         process.stdout.write("*");  
@@ -10,4 +11,63 @@ for(let i=1;i<=n;i++){
 }
 
 
+// Q2.Right trianglen of Alphabets.
 
+for(let i=1;i<=n;i++){
+    let ascci=65;
+ for(let j=1;j<=i;j++){
+        process.stdout.write(String.fromCharCode(ascci));  
+        ascci++;
+ }
+  
+ console.log();
+}
+
+
+
+// // Q3.Triangle  Pattern;
+
+for(let i=1;i<=n;i++){
+ for(let j=n-1;j>=i;j--){
+        process.stdout.write(" ");  
+ }
+  for(let k=1;k<=i;k++){
+        process.stdout.write("*");  
+ }
+   for(let l=1;l<=i-1;l++){
+        process.stdout.write("*");  
+ }
+ 
+ console.log();
+}
+
+
+// // Q4. reverse right Triangle  Pattern;
+
+for(let i=1;i<=n;i++){
+ for(let j=n-1;j>=i;j--){
+        process.stdout.write(" ");  
+ }
+  for(let k=1;k<=i;k++){
+        process.stdout.write("*");  
+ }
+ 
+ console.log();
+}
+
+
+// // Q5. X   Pattern;
+
+for(let i=1;i<=n;i++){
+    for(let j=1;j<=n;j++){
+   if(i==j || i+j==n+1  ) {
+      process.stdout.write("*");  
+    }else {
+  process.stdout.write("-");  
+      
+    }
+    
+}
+console.log();
+
+}
