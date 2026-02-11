1b37e311b7c213d2a3ec061abc337714c151d1e3
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-11 10:07:20 +0500

    add

--- pattern.js
+++ pattern.js
@@ -1,3 +1,13 @@
 let prompt=require('prompt-sync')();
-let a=prompt("enter a number");
-process.stdout.write(a);
+let n=prompt("enter a number");
+
+// Q1.Right Pattern;
+for(let i=1;i<=n;i++){
+ for(let j=1;j<=i;j++){
+        process.stdout.write("*");  
+ }
+ console.log();
+}
+
+
+
