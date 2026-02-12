bb580cd09ab7b7f3540d20b1dd29866ffce123ce
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-12 09:34:09 +0500

    add questions

--- pattern.js
+++ pattern.js
@@ -71,3 +71,20 @@ for(let i=1;i<=n;i++){
 console.log();
 
 }
+
+
+// // // Q6. X   Pattern;
+for(let i=1;i<=n;i++){
+    for(let j=1;j<=n*2;j++){
+    if(i==j || i+j==n*2+1) {
+      process.stdout.write("*");  
+    } else {
+  process.stdout.write("-");        
+    }
+    
+}
+console.log();
+
+}
+
+
