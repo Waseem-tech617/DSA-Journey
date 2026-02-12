c7dbb3ad1fe6f603afee2e287394d0eb8c381ef6
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-12 09:35:09 +0500

    add questions

--- pattern.js
+++ pattern.js
@@ -1,90 +1,90 @@
-let prompt=require('prompt-sync')();
-let n=Number(prompt("enter a number"));
+// let prompt=require('prompt-sync')();
+// let n=Number(prompt("enter a number"));
 
-// Q1.Right triangle;
+// // Q1.Right triangle;
 
-for(let i=1;i<=n;i++){
- for(let j=1;j<=i;j++){
-        process.stdout.write("*");  
- }
- console.log();
-}
+// for(let i=1;i<=n;i++){
+//  for(let j=1;j<=i;j++){
+//         process.stdout.write("*");  
+//  }
+//  console.log();
+// }
 
 
-// Q2.Right trianglen of Alphabets.
+// // Q2.Right trianglen of Alphabets.
 
-for(let i=1;i<=n;i++){
-    let ascci=65;
- for(let j=1;j<=i;j++){
-        process.stdout.write(String.fromCharCode(ascci));  
-        ascci++;
- }
+// for(let i=1;i<=n;i++){
+//     let ascci=65;
+//  for(let j=1;j<=i;j++){
+//         process.stdout.write(String.fromCharCode(ascci));  
+//         ascci++;
+//  }
   
- console.log();
-}
+//  console.log();
+// }
 
 
 
-// // Q3.Triangle  Pattern;
+// // // Q3.Triangle  Pattern;
 
-for(let i=1;i<=n;i++){
- for(let j=n-1;j>=i;j--){
-        process.stdout.write(" ");  
- }
-  for(let k=1;k<=i;k++){
-        process.stdout.write("*");  
- }
-   for(let l=1;l<=i-1;l++){
-        process.stdout.write("*");  
- }
+// for(let i=1;i<=n;i++){
+//  for(let j=n-1;j>=i;j--){
+//         process.stdout.write(" ");  
+//  }
+//   for(let k=1;k<=i;k++){
+//         process.stdout.write("*");  
+//  }
+//    for(let l=1;l<=i-1;l++){
+//         process.stdout.write("*");  
+//  }
  
- console.log();
-}
+//  console.log();
+// }
 
 
-// // Q4. reverse right Triangle  Pattern;
+// // // Q4. reverse right Triangle  Pattern;
 
-for(let i=1;i<=n;i++){
- for(let j=n-1;j>=i;j--){
-        process.stdout.write(" ");  
- }
-  for(let k=1;k<=i;k++){
-        process.stdout.write("*");  
- }
+// for(let i=1;i<=n;i++){
+//  for(let j=n-1;j>=i;j--){
+//         process.stdout.write(" ");  
+//  }
+//   for(let k=1;k<=i;k++){
+//         process.stdout.write("*");  
+//  }
  
- console.log();
-}
+//  console.log();
+// }
 
 
-// // Q5. X   Pattern;
+// // // Q5. X   Pattern;
 
-for(let i=1;i<=n;i++){
-    for(let j=1;j<=n;j++){
-   if(i==j || i+j==n+1  ) {
-      process.stdout.write("*");  
-    }else {
-  process.stdout.write("-");  
+// for(let i=1;i<=n;i++){
+//     for(let j=1;j<=n;j++){
+//    if(i==j || i+j==n+1  ) {
+//       process.stdout.write("*");  
+//     }else {
+//   process.stdout.write("-");  
       
-    }
+//     }
     
-}
-console.log();
+// }
+// console.log();
 
-}
+// }
 
 
-// // // Q6. X   Pattern;
-for(let i=1;i<=n;i++){
-    for(let j=1;j<=n*2;j++){
-    if(i==j || i+j==n*2+1) {
-      process.stdout.write("*");  
-    } else {
-  process.stdout.write("-");        
-    }
+// // // // Q6. X   Pattern;
+// for(let i=1;i<=n;i++){
+//     for(let j=1;j<=n*2;j++){
+//     if(i==j || i+j==n*2+1) {
+//       process.stdout.write("*");  
+//     } else {
+//   process.stdout.write("-");        
+//     }
     
-}
-console.log();
+// }
+// console.log();
 
-}
+// }
 
 
