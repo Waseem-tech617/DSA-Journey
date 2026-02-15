b951a2de20a23cd60f4b7c87f1e172c37fc4478a
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-15 12:49:43 +0500

    add questions

--- arrays.js
+++ arrays.js
@@ -4,3 +4,18 @@ for(let i=0;i<arr.length;i++){
  arr[i]=prompt("ener a vlaue");
 }
 console.log(arr);
+
+// Q2.Find 2nd min number.
+let arr = [56,56, 34, 4, 27, 444,444,444,444, 3,55];
+let fisrtmin = Math.min(arr[0],arr[1]);
+let secondmin = Math.max(arr[0],arr[1]);
+for (let i = 2; i < arr.length; i++) {
+    if (arr[i] < fisrtmin) {
+        secondmin=fisrtmin;
+        fisrtmin= arr[i];
+    }else if(arr[i]<secondmin && fisrtmin!=arr[i]){
+        secondmin=arr[i];
+    }
+     
+}
+console.log(secondmin); 
