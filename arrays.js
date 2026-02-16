b8c8bf04da9d7fdeaedd5a86c6ea1ddee416425d
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-16 12:46:57 +0500

    add

--- arrays.js
+++ arrays.js
@@ -63,3 +63,13 @@ for(let i=0;i<=arr.length-1;i++){
 arr[arr.length-1]=copy;
 
 console.log(arr);
+
+
+// Q7.Right rotation by element by 1.
+let arr=[1,2,3,4,5,6];
+let copy=arr[arr.length-1];
+for(let i=arr.length-1;i>=0;i--){
+    arr[i]=arr[i-1];
+}
+arr[0]=copy;
+console.log(arr);
