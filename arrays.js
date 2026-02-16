7b06c67797aa0f8bf7d7f65524cbf5ece966afce
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-16 11:38:40 +0500

    add

--- arrays.js
+++ arrays.js
@@ -28,3 +28,14 @@ for(let i=arr.length-1;i>=0;i--){
 }
  console.log(temp);
 
+//  Q4.Reverse of array without space.
+let arr=[1,2,3,4,5];
+let i=0 , j=arr.length-1;
+while(i!=j){
+    let temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+    i++;
+    j--;
+}
+console.log(arr);
