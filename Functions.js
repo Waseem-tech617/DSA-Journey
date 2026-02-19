4d4f083cf6c298e6c664bfbd444d3042baaac295
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-19 15:17:21 +0500

    add questions

---
+++ Functions.js
@@ -0,0 +1,11 @@
+function doubel(number){
+    return number*2;
+}
+let numbers=[1,2,3,4,5];
+let doubels=numbers.map((dol)=>{
+    return dol*2;
+});
+
+consol.log(doubels);
+
+
--- arrays.js
+++ arrays.js
@@ -1,134 +1,158 @@
-// Q1.Getting elements in arrays through prompt.
-let arr = new Array(5);
-for (let i = 0; i < arr.length; i++) {
-    arr[i] = prompt("ener a vlaue");
-}
-console.log(arr);
+// // Q1.Getting elements in arrays through prompt.
+// let arr = new Array(5);
+// for (let i = 0; i < arr.length; i++) {
+//     arr[i] = prompt("ener a vlaue");
+// }
+// console.log(arr);
 
-// Q2.Find 2nd min number.
-let arr = [56, 56, 34, 4, 27, 444, 444, 444, 444, 3, 55];
-let fisrtmin = Math.min(arr[0], arr[1]);
-let secondmin = Math.max(arr[0], arr[1]);
-for (let i = 2; i < arr.length; i++) {
-    if (arr[i] < fisrtmin) {
-        secondmin = fisrtmin;
-        fisrtmin = arr[i];
-    } else if (arr[i] < secondmin && fisrtmin != arr[i]) {
-        secondmin = arr[i];
-    }
+// // Q2.Find 2nd min number.
+// let arr = [56, 56, 34, 4, 27, 444, 444, 444, 444, 3, 55];
+// let fisrtmin = Math.min(arr[0], arr[1]);
+// let secondmin = Math.max(arr[0], arr[1]);
+// for (let i = 2; i < arr.length; i++) {
+//     if (arr[i] < fisrtmin) {
+//         secondmin = fisrtmin;
+//         fisrtmin = arr[i];
+//     } else if (arr[i] < secondmin && fisrtmin != arr[i]) {
+//         secondmin = arr[i];
+//     }
 
-}
-console.log(secondmin);
+// }
+// console.log(secondmin);
 
-//  Q3.Reverse of array.
-let arr = [1, 2, 3, 4, 5];
-let temp = [];
-for (let i = arr.length - 1; i >= 0; i--) {
-    temp.push(arr[i]);
-}
-console.log(temp);
+// //  Q3.Reverse of array.
+// let arr = [1, 2, 3, 4, 5];
+// let temp = [];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     temp.push(arr[i]);
+// }
+// console.log(temp);
 
-//  Q4.Reverse of array without space.
-let arr = [1, 2, 3, 4, 5];
-let i = 0, j = arr.length - 1;
-while (i != j) {
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-    i++;
-    j--;
-}
-console.log(arr);
+// //  Q4.Reverse of array without space.
+// let arr = [1, 2, 3, 4, 5];
+// let i = 0, j = arr.length - 1;
+// while (i != j) {
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     i++;
+//     j--;
+// }
+// console.log(arr);
 
-// Q5.Move the negitive elements to left side of array .
-let arr = [1, 0, 1, 0, -1, -1, -1, 0];
-let i = 0, j = 0;
-while (i < arr.length) {
-    if (arr[i] < 0) {
-        let temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-        j++;
-    }
-    i++;
-}
-console.log(arr);
+// // Q5.Move the negitive elements to left side of array .
+// let arr = [1, 0, 1, 0, -1, -1, -1, 0];
+// let i = 0, j = 0;
+// while (i < arr.length) {
+//     if (arr[i] < 0) {
+//         let temp = arr[j];
+//         arr[j] = arr[i];
+//         arr[i] = temp;
+//         j++;
+//     }
+//     i++;
+// }
+// console.log(arr);
 
-// Q6.left rotation by element by 2.
-let arr = [1, 2, 3, 4, 5];
-let copy = arr[0];
-let copy2 = arr[1];
-for (let i = 0; i <= arr.length - 1; i++) {
-    arr[i] = arr[i + 2];
-}
-arr[arr.length - 2] = copy;
-arr[arr.length - 1] = copy2;
+// // Q6.left rotation by element by 2.
+// let arr = [1, 2, 3, 4, 5];
+// let copy = arr[0];
+// let copy2 = arr[1];
+// for (let i = 0; i <= arr.length - 1; i++) {
+//     arr[i] = arr[i + 2];
+// }
+// arr[arr.length - 2] = copy;
+// arr[arr.length - 1] = copy2;
 
-console.log(arr);
-
-
-// Q7.Right rotation by element by 1.
-let arr = [1, 2, 3, 4, 5, 6];
-let copy = arr[arr.length - 1];
-for (let i = arr.length - 1; i >= 0; i--) {
-    arr[i] = arr[i - 1];
-}
-arr[0] = copy;
-console.log(arr);
+// console.log(arr);
 
 
-
-// Q8.left rotation by element by 1.
-let arr = [1, 2, 3, 4, 5];
-let copy = arr[0];
-for (let i = 0; i <= arr.length - 1; i++) {
-    arr[i] = arr[i + 1];
-}
-arr[arr.length - 2] = copy;
-console.log(arr);
+// // Q7.Right rotation by element by 1.
+// let arr = [1, 2, 3, 4, 5, 6];
+// let copy = arr[arr.length - 1];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     arr[i] = arr[i - 1];
+// }
+// arr[0] = copy;
+// console.log(arr);
 
 
 
-// // Q9.left rotation by element by k element.
-let arr = [1, 2, 3, 4, 5];
-let k = prompt("enter a k value");
-k = k % arr.length;
-for (let i = 1; i <= k; i++) {
-    let copy = arr[0];
-    for (let j = 0; j <= arr.length - 1; j++) {
-        arr[j] = arr[j + 1];
-    }
-    arr[arr.length - 1] = copy;
-}
-console.log(arr);
-k = k % arr.length;
+// // Q8.left rotation by element by 1.
+// let arr = [1, 2, 3, 4, 5];
+// let copy = arr[0];
+// for (let i = 0; i <= arr.length - 1; i++) {
+//     arr[i] = arr[i + 1];
+// }
+// arr[arr.length - 2] = copy;
+// console.log(arr);
 
-// // Q9.left rotation by  k element by space.
-let arr = [1, 2, 3, 4, 5];
-let k = Number(prompt("enter a k value"));
-let array = new Array(arr.length);
-for (let i = 0; i <= arr.length - 1; i++) {
-    array[i] = arr[(i + k) % arr.length]
-}
-console.log(array);
 
-// // Q10.right rotation by  k element creating without space.
-let arr = [1, 2, 3, 4, 5];
-let k = Number(prompt("enter a k value"));
-k = k % arr.length;
-Reverse(0, arr.length - 1);
-Reverse(0, k - 1);
-Reverse(k, arr.length - 1);
-console.log(arr);
-function Reverse(i, j) {
-    while (i < j) {
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        i++;
-        j--;
-    }
-}
+
+// // // Q9.left rotation by element by k element.
+// let arr = [1, 2, 3, 4, 5];
+// let k = prompt("enter a k value");
+// k = k % arr.length;
+// for (let i = 1; i <= k; i++) {
+//     let copy = arr[0];
+//     for (let j = 0; j <= arr.length - 1; j++) {
+//         arr[j] = arr[j + 1];
+//     }
+//     arr[arr.length - 1] = copy;
+// }
+// console.log(arr);
+// k = k % arr.length;
+
+// // // Q9.left rotation by  k element by space.
+// let arr = [1, 2, 3, 4, 5];
+// let k = Number(prompt("enter a k value"));
+// let array = new Array(arr.length);
+// for (let i = 0; i <= arr.length - 1; i++) {
+//     array[i] = arr[(i + k) % arr.length]
+// }
+// console.log(array);
+
+// // // Q10.right rotation by  k element creating without space.
+// let arr = [1, 2, 3, 4, 5];
+// let k = Number(prompt("enter a k value"));
+// k = k % arr.length;
+// Reverse(0, arr.length - 1);
+// Reverse(0, k - 1);
+// Reverse(k, arr.length - 1);
+// console.log(arr);
+// function Reverse(i, j) {
+//     while (i < j) {
+//         let temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//         i++;
+//         j--;
+//     }
+// }
+// Remove duplicate from sorted array.
+
+// let arr=[0,0,1,1,1,2,2,3,3];
+// function removeduplicates(arr){
+// let j=1;
+// for(let i=0;i<arr.length-1;i++){
+//     if(arr[i]!=arr[i+1]){
+//        arr[j]=arr[i+1];
+//        j++;
+//     }
+ 
+// }
+
+// return j;
+
+// }
+
+// let length = removeduplicates(arr);
+// console.log(arr.slice(0, length));
+
+
+
+
+
 
 
 
