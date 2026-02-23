ee0774ed4a250fc9285a2b78322fdb19b7ed0b67
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-23 10:23:20 +0500

    add

--- Functions.js
+++ Functions.js
@@ -1,4 +1,4 @@
-Q1.filter greater then 10.
+// Q1.filter greater then 10.
 
 let numbers = [5, 12, 8, 130, 44];
 let filter=numbers.filter(greater=> greater>10);
@@ -48,6 +48,53 @@ console.log(doubel);
 let product = {id: 1, title: "Laptop", price: 50000};
 function showProduct({id,title,price}){
     console.log(id,title);
-    
+
 }
 showProduct(product);
+
+
+
+// Q7. Function Declaration .Create a function greetUser that takes a name as parameter and returns "Hello, <name>!".
+
+function greetuser(name) {
+    return `Hello ${name}`;
+}
+
+ // Q8. Function Expression .Write a function expression called square that returns the square of a number.
+
+let square = function (number) {
+    return number * number;
+}
+
+// Q9. Arrow Function .Convert the following into an arrow function:
+
+let multiply = (a, b) => a * b;
+
+
+// Q10. Default Parameter.Write a function sayHi that prints "Hi, Guest!" if no name is provided, otherwise prints "Hi, <name>!".
+function sayhi(name="Guest!"){
+console.log( `Hi,${name}`);
+}
+
+
+ // Q11. Function in Object. Create an object calculator with methods add and subtract that take two numbers and return the result.
+
+const calculator = {
+  add: function(a,b) {
+    return a + b;
+  },
+  subtract:function(a,b) {
+    return a - b;
+  }
+};
+console.log(calculator.add(1,2));//3
+
+
+
+ // Q12. Using Array with Function. Given numbers = [1, 2, 3, 4], create a new array doubled where each number is multiplied by 2 using map and an arrow function.
+let numbers = [1, 2, 3, 4];
+let double=numbers.map(val => val*2);
+console.log(double);
+
+
+
